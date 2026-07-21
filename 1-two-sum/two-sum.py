5ac9cc41e8b160c859c78fc9b050f2e1c6b93b13
Preeti Ganiger <preetiganiger1111@gmail.com>
2026-07-21 12:54:19 +0530

    Time: 2435 ms (7.97%) | Memory: 13.2 MB (35.89%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+       
+        n=len(nums)
+        for i in range (0,n):
+            for j in range (i+1,n):
+                sum = nums[i]+nums[j]
+                if sum==target:
+                    return [i,j]
+                else:
+                    continue
+        
